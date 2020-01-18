7b81e81c13737ad864e5f07d1985aad82c8bd292
gregwhitaker <gwhitake@gmail.com>
2020-01-17 22:06:56 -0800

    Doing work

--- number-client/src/main/java/example/number/client/NumberClient.java
+++ number-client/src/main/java/example/number/client/NumberClient.java
@@ -1,12 +1,54 @@
 package example.number.client;
 
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class NumberClient {
     private static final Logger LOG = LoggerFactory.getLogger(NumberClient.class);
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        RSocket rSocket = RSocketFactory.connect()
+                .transport(TcpClientTransport.create(7000))
+                .start()
+                .block();
 
+        Random rand = new Random(System.currentTimeMillis());
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Flux<Payload> intPayloads = Flux.range(1, 50)
+                .delayElements(Duration.ofMillis(500))
+                .map(cnt -> rand.nextInt(11))
+                .map(i -> {
+                    LOG.info("Sending: {}", i);
+                    return DefaultPayload.create(BigInteger.valueOf(i).toByteArray());
+                })
+                .doOnComplete(() -> {
+                    LOG.info("Done");
+                    latch.countDown();
+                });
+
+        rSocket.requestChannel(intPayloads)
+                .subscribe(payload -> {
+                    byte[] bytes = new byte[payload.data().readableBytes()];
+                    payload.data().readBytes(bytes);
+
+                    long total = new BigInteger(bytes).longValue();
+
+                    LOG.info("Total Even Number Count: {}", total);
+                });
+
+        latch.await();
     }
 }
