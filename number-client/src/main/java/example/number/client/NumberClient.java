883368023f87120a977a6224637594b50cfce272
gregwhitaker <gwhitake@gmail.com>
2020-01-17 21:16:24 -0800

    Doing work

---
+++ number-client/src/main/java/example/number/client/NumberClient.java
@@ -0,0 +1,12 @@
+package example.number.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NumberClient {
+    private static final Logger LOG = LoggerFactory.getLogger(NumberClient.class);
+
+    public static void main(String... args) {
+        
+    }
+}
---
+++ number-service/src/main/java/example/number/service/NumberService.java
@@ -0,0 +1,12 @@
+package example.number.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NumberService {
+    private static final Logger LOG = LoggerFactory.getLogger(NumberService.class);
+
+    public static void main(String... args) {
+
+    }
+}
